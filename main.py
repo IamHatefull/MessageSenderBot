eac706b2171bf2b02f098cd22367758e1b8485c5
Borys Denysenko <48831975+IamHatefull@users.noreply.github.com>
2023-05-29 23:08:17 +0300

    stateform update

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from core.handlers.basic import get_start, get_photo, get_hello, get_location
 from core.handlers.basic import show_keyboard, get_inline
 from core.handlers.contact import get_fake_contact, get_true_contact
 from core.handlers.callback import select_macbook
+from core.handlers import form
 from core.filters.iscontact import IsTrueContact
 from core.utils.commands import set_commands
 from core.middlewares.dbmiddleware import DBSession
@@ -46,6 +47,7 @@ async def start():
     dp.startup.register(start_bot)
     dp.shutdown.register(stop_bot)
     dp.callback_query.register(select_macbook, F.data.startswith('apple_'))
+    dp.message.register(form.get_form, Command(commands='form'))
     dp.message.register(get_start, Command(commands=['start', 'run']))
     dp.message.register(show_keyboard, Command(commands=['show']))
     dp.message.register(get_inline, Command(commands='inline'))
