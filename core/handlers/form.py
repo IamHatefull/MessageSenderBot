62b35f977bfd8c21c777115565358a051a36f1ed
Borys Denysenko <48831975+IamHatefull@users.noreply.github.com>
2023-05-31 23:04:25 +0300

    Update form.py

--- core/handlers/form.py
+++ core/handlers/form.py
@@ -6,6 +6,6 @@ async def get_form(message: Message, state: FSMContext):
     await message.answer(f"{message.from_user.first_name}, let's fill a quiz. Enter your name:")
     await state.set_state(StepsForm.GET_NAME)
 
-
+# State function that expect your name
 async def get_name(message: Message):
     await message.answer(f'Your first name:\r\n{message.text}\r\nYour last name')
