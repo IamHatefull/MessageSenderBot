4d72ef533f7a4a7af32e8f63807a879977656085
Borys Denysenko <48831975+IamHatefull@users.noreply.github.com>
2023-06-03 23:19:55 +0300

    Update form.py

--- core/handlers/form.py
+++ core/handlers/form.py
@@ -17,3 +17,16 @@ async def get_last_name(message: Message, state: FSMContext):
     await message.answer(f'Your last name:\r\n{message.text}\r\n now enter your age.')
     await state.update_data(last_name=message.text)
     await state.set_state(StepsForm.GET_AGE)
+
+async def get_age(message: Message, state: FSMContext):
+    await message.answer(f'Your age:\r\n{message.text}\r\n')
+    context_data = await state.get_data()
+    await message.answer(f'Saved data in state machine:\r\n{str(context_data)}')
+    name = context_data.get('name')
+    last_name = context_data.get('last_name')
+    data_user = f'Here is your data\r\n'\
+                f'Name {name}'\
+                f'Last name {last_name}'\
+                f'Age {message.text}'
+    await message.answer(data_user)
+    await state.clear()
