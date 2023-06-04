7f2b17343e98df80410bbfd793984f19168a08c0
Borys Denysenko <48831975+IamHatefull@users.noreply.github.com>
2023-06-04 23:26:53 +0300

    Update contact.py

--- core/handlers/contact.py
+++ core/handlers/contact.py
@@ -1,8 +1,10 @@
 from aiogram.types import Message
 from aiogram import Bot
 
+# Function will send you an answer if the contact you provided is yours
 async def get_true_contact(message: Message, bot: Bot):
     await message.answer(f'You send <b>your</b> contact.')
 
+# Function will send you an answer if the contact you provided is not yours
 async def get_fake_contact(message: Message, bot: Bot):
     await message.answer(f'You send <b>not your</b> contact.')
