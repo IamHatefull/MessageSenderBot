68ed4462fb5736ee95381ebd28b61d0d4fcce36e
Borys Denysenko <48831975+IamHatefull@users.noreply.github.com>
2023-06-10 21:27:18 +0300

    Update sender.py

--- core/handlers/sender.py
+++ core/handlers/sender.py
@@ -1,6 +1,8 @@
 from aiogram import Message
 from aiogram import Bot
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import CommandObject
 
 
-async def get_sender():
+async def get_sender(message: Message, command: CommandObject, state: FSMContext):
     pass
