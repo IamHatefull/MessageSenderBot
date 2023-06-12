fdc8ad75a243a3d727f9484f1c6b403b0e3b9eb0
Borys Denysenko <48831975+IamHatefull@users.noreply.github.com>
2023-06-12 22:58:32 +0300

    Update sender.py

--- core/handlers/sender.py
+++ core/handlers/sender.py
@@ -6,4 +6,9 @@ from aiogram.filters import CommandObject
 
 async def get_sender(message: Message, command: CommandObject, state: FSMContext):
     if not command.args:
-        await message.answer(f'')
+        await message.answer(f'Enter /sender command and sending name')
+        return
+    
+    await message.answer(f'')
+    await state.update_data(name_camp=command.args)
+    await state.set_state()
